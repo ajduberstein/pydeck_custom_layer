f6fd950cf45e40535e78c15cfe08500ad8b5b64f
Andrew Duberstein <duberstein@uber.com>
2020-04-16 18:06:26 -0700

    Update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-export { LabeledGeoJsonLayer } from "./labeled-geojson-layer";
+/* global window, global */
+import { LabeledGeoJsonLayer } from "./labeled-geojson-layer";
+
+const _global = typeof window === 'undefined' ? global : window;
+_global.LabeledGeoJsonLayerLibrary = {LabeledGeoJsonLayer};
+
+export default LabeledGeoJsonLayerLibrary;
+
